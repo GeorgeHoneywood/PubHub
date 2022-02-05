ccfbedb1e7f5e1042e8e7500d06874586b414e03
Samuel Sandoval <sjsandoval1000@gmail.com>
2022-02-05 14:54:52 +0000

    Added user location data collection functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl, { Map } from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import ReactMapGL, { Marker } from "react-map-gl";
 
 // this is fine :))
 mapboxgl.accessToken = 'pk.eyJ1IjoiaG9uZXlmb3giLCJhIjoiY2t6OXVicGU2MThyOTJvbnh1a21idjhkZSJ9.LMyDoR9cFGG3HqAc9Zlwkg';
+
 
 function App() {
   const mapContainer = useRef(null);
@@ -13,16 +15,28 @@ function App() {
   const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    //if (map.current) return; // initialize map only once
     // FIXME: these ts ignores, see https://stackoverflow.com/questions/60322612/what-are-the-correct-types-to-map-variables-using-mapbox-in-react/60334542#60334542
     // @ts-ignore
-    map.current = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
       // @ts-ignore
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom,
     });
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+      })
+    );
+
   });
 
   return (
