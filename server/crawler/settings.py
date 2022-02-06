c6df4f7483e7512cf76ab0489384b7a8fb2a1f61
GeorgeHoneywood <george.honeywood.2019@rhul.ac.uk>
2022-02-06 08:17:37 +0000

    fix(server): missing https:// on COR allow

--- server/crawler/settings.py
+++ server/crawler/settings.py
@@ -82,7 +82,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://pub-hub.netlify.app",
-    "pub-hub.tech",
+    "https://pub-hub.tech",
 ]
 
 # Database
