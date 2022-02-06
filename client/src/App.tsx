809f09fd89dcbd0797086c7ef57d327023d6fb90
GeorgeHoneywood <george.honeywood.2019@rhul.ac.uk>
2022-02-06 06:57:49 +0000

    chore(client): build error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
-import React, {useState} from 'react';
+import {useState} from 'react';
 import {MapView} from './components/map-view/MapView';
 import {TopBar} from "./components/TopBar";
 import {CurrentCrawl} from "./contexts/CurrentCrawl";
 import {CurrentCrawlModel} from "./models/CurrentCrawlModel";
 import {LoadingContext} from "./contexts/LoadingContext";
 import {LoadingOverlay} from "./components/loading-overlay/LoadingOverlay";
-import {PubList} from "./components/pub-list/PubList";
 import {MaxPubs} from './contexts/MaxPubs';
 
 function App() {
