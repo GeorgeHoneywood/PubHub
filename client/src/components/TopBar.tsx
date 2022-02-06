4a4949b5c62eebc6761155929657b755db2e6550
GeorgeHoneywood <george.honeywood.2019@rhul.ac.uk>
2022-02-06 00:16:30 +0000

    feat(client): add logo to navbar

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,13 +1,20 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
-
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../logo.svg";
 
 export function TopBar(props: any) {
-
-
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top me-2"
+                    />
+                    PubHub
+                </Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
